53513c626eeea857bb895dfe52a6ed9d28bdb6cf
SteveWW <s@steveww.org>
2021-10-28 10:02:50 +0100

    dry run switch

--- gitops.sh
+++ gitops.sh
@@ -1,20 +1,42 @@
 #!/bin/sh
 
+usage() {
+    echo "$(basename $0) -t [prod|staging]"
+    echo ""
+    echo "-t = test dry run"
+    echo ""
+    exit 1
+}
+
+unset DRY_RUN
+while getopts 't' o
+do
+    case "$o" in
+        t)
+            DRY_RUN="--dry-run"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $(expr $OPTIND - 1)
+
 case "$1" in
     prod|staging)
         STAGE=$1
         ;;
     *)
-        echo "stage required as first arg [prod|staging]"
-        exit 1
+        usage
         ;;
 esac
 
 NAME="podinfo-$STAGE"
 
 gitops app add \
+    --app-config-url git@github.com:steveww/gitops \
     --url git@github.com:steveww/podinfo-deploy \
     --path $STAGE \
     --name $NAME \
-    --dry-run
+    $DRY_RUN
 
