1ea242842c71699a00e1eec5e84992bc38f4d6b6
SteveWW <s@steveww.org>
2021-11-03 11:48:04 +0000

    more load

--- loadgen/load.sh
+++ loadgen/load.sh
@@ -11,7 +11,7 @@ fi
 while true
 do
     echo "$LOAD_URL_BASE"
-    ./hey -c 1 -q 2 -z 10s -m GET -t 3 "$LOAD_URL_BASE"
+    ./hey -c 2 -q 4 -z 10s -m GET -t 3 "$LOAD_URL_BASE"
     echo "${LOAD_URL_BASE}/echo"
-    ./hey -c 1 -q 2 -z 10s -m POST -t 3 -d "Hello World" "${LOAD_URL_BASE}/echo"
+    ./hey -c 2 -q 4 -z 10s -m POST -t 3 -d "Hello World" "${LOAD_URL_BASE}/echo"
 done
